1ba92c6ed4f8f38bed0e1ec19419f260f26cb08d
TAT <TAT@plugcubed.net>
2014-12-07 03:07:15 +0100

    Update API and added Lang

--- API.js
+++ API.js
@@ -1,86 +1,134 @@
+//noinspection JSUnusedGlobalSymbols
 var API = {
     CHAT: 'chat',
+    FRIEND_JOIN: 'friendJoin',
     USER_SKIP: 'userSkip',
     USER_JOIN: 'userJoin',
     USER_LEAVE: 'userLeave',
-    USER_FAN: 'userFan',
-    FRIEND_JOIN: 'friendJoin',
-    FAN_JOIN: 'fanJoin',
     VOTE_UPDATE: 'voteUpdate',
-    CURATE_UPDATE: 'curateUpdate',
-    ROOM_SCORE_UPDATE: 'roomScoreUpdate',
-    DJ_ADVANCE: 'djAdvance',
-    DJ_UPDATE: 'djUpdate',
-    VOTE_SKIP: 'voteSkip',
+    GRAB_UPDATE: 'grabUpdate',
+    SCORE_UPDATE: 'scoreUpdate',
+    ADVANCE: 'advance',
     MOD_SKIP: 'modSkip',
     WAIT_LIST_UPDATE: 'waitListUpdate',
     CHAT_COMMAND: 'chatCommand',
     HISTORY_UPDATE: 'historyUpdate',
     ROLE: {
-        ADMIN: 10
-        AMBASSADOR: 8
-        BOUNCER: 2
-        COHOST: 4
+        NONE: 0,
+        DJ: 1,
+        BOUNCER: 2,
+        MANAGER: 3,
+        COHOST: 4,
         HOST: 5
-        MANAGER: 3
-        NONE: 0
-        RESIDENTDJ: 1
     },
     STATUS: {
         AVAILABLE: 0,
-        AFK: 1,
+        AWAY: 1,
         WORKING: 2,
         GAMING: 3
     },
     BAN: {
-        HOUR: 60,
-        DAY: 1440,
-        PERMA: -1
+        HOUR: "h",
+        DAY: "d",
+        PERMA: "f"
     },
-    enabled: false,
-    on: function() {},
-    once: function() {},
-    off: function() {},
-    trigger: function() {},
-    stopListening: function() {},
-    listenTo: function() {},
-    listenToOnce: function() {},
-    bind: function() {},
-    unbind: function() {},
-    getUsers: function() {},
-    getUser: function() {},
-    getAudience: function() {},
-    getDJ: function() {},
-    getStaff: function() {},
-    getAdmins: function() {},
-    getAmbassadors: function() {},
-    getHost: function() {},
-    getMedia: function() {},
-    getTimeElapsed: function() {},
-    getTimeRemaining: function() {},
-    getWaitList: function() {},
-    getRoomScore: function() {},
-    getHistory: function() {},
-    hasPermission: function() {},
-    setVolume: function() {},
-    getVolume: function() {},
-    getWaitListPosition: function() {},
-    setStatus: function() {},
-    sendChat: function() {},
-    chatLog: function() {},
-    djJoin: function() {},
-    djLeave: function() {},
-    getNextMedia: function() {},
-    getBannedUsers: function() {},
-    moderateForceSkip: function() {},
-    moderateAddDJ: function() {},
-    moderateRemoveDJ: function() {},
-    moderateBanUser: function() {},
-    moderateUnbanUser: function() {},
-    moderateDeleteChat: function() {},
-    moderateSetRole: function() {},
-    moderateLockWaitList: function() {},
-    moderateMoveDJ: function() {},
-    dispatch: function() {},
-    _events: {}
+    MUTE: {
+        SHORT: "s",
+        MEDIUM: "m",
+        LONG: "l"
+    },
+    enabled: true,
+    _events: {},
+    on: function(e, t, n) {
+    },
+    once: function(e, t, r) {
+    },
+    off: function(e, t, r) {
+    },
+    trigger: function(e) {
+    },
+    stopListening: function(e, t, r) {
+    },
+    listenTo: function(t, r, i) {
+    },
+    listenToOnce: function(t, r, i) {
+    },
+    bind: function(e, t, n) {
+    },
+    unbind: function(e, t, r) {
+    },
+    dispatch: function(t, n) {
+    },
+    sendChat: function(n) {
+    },
+    chatLog: function(t, n) {
+    },
+    djJoin: function() {
+    },
+    djLeave: function() {
+    },
+    getAdmins: function() {
+    },
+    getAmbassadors: function() {
+    },
+    getAudience: function() {
+    },
+    getBannedUsers: function() {
+    },
+    getDJ: function() {
+    },
+    getHistory: function() {
+    },
+    getHost: function() {
+    },
+    getMedia: function() {
+    },
+    getNextMedia: function() {
+    },
+    getScore: function() {
+    },
+    getStaff: function() {
+    },
+    getTimeElapsed: function() {
+    },
+    getTimeRemaining: function() {
+    },
+    getUser: function(r) {
+    },
+    getUsers: function() {
+    },
+    getWaitList: function() {
+    },
+    getWaitListPosition: function(i) {
+    },
+    hasPermission: function(r, i) {
+    },
+    moderateForceSkip: function() {
+    },
+    moderateAddDJ: function(t) {
+    },
+    moderateRemoveDJ: function(t) {
+    },
+    moderateBanUser: function(t, n, s) {
+    },
+    moderateUnbanUser: function(t) {
+    },
+    moderateDeleteChat: function(t) {
+    },
+    moderateSetRole: function(t, n) {
+    },
+    moderateLockWaitList: function(e, t) {
+    },
+    moderateMoveDJ: function(e, t) {
+    },
+    moderateMuteUser: function(t, n, s) {
+    },
+    moderateUnmuteUser: function(t) {
+    },
+    setStatus: function(e) {
+    },
+    setVolume: function(n) {
+    },
+    getVolume: function() {
+    }
 };
